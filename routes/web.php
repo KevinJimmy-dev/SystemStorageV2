4f395c702ad9f6af092334475184c7dc930d9ac4
Kevin Jimmy <kevinjimmy.profissional@homtail.com>
2022-03-23 12:49:48 -0300

    Nova rota para o login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [UserController::class, 'index']);
+Route::post('/auth', [UserController::class, 'auth']);
