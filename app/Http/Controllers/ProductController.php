36b2b7a0c5aae23dd19ef7733e01b2e622589886
Kevin Jimmy <kevinjimmy.profissional@homtail.com>
2022-04-05 17:25:47 -0300

    Agora da

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,6 +10,7 @@ use App\Models\{
     Categorie,
     Control
 };
+use Exception;
 use Illuminate\Support\Arr;
 
 class ProductController extends Controller{
@@ -166,14 +167,63 @@ class ProductController extends Controller{
             echo "<li>Nenhum produto encontrado...</li>";
 
         } else{
-            foreach($products as $product){                
-                echo "<li>
-                        $product[name] 
-                        <button class='btn-add' id='$product[id]' name='$product[name]' onclick='add(id, name, $product[quantity]);'>
-                            <i class='fa-solid fa-plus direita'></i>
-                        </button>
-                    </li>"; 
+
+            foreach($products as $product){  
+
+                echo "
+                        <li>
+                            $product[name] 
+                            <button class='btn-add' id='$product[id]' name='$product[name]' onclick='add(id, name, $product[quantity], `$product[storageUnity]`);'>
+                                <i class='fa-solid fa-plus direita'></i>
+                            </button>
+                        </li>
+                    "; 
             }  
+        }
+    }
+
+    public function request(Request $request){
+        
+        $quantity = $request->quantity;
+        $requests = $request->request_value;
+        $id = $request->id_product;
+        $name = $request->name_product;
+
+        try{
+            if(empty($quantity)){
+
+                return redirect()->route('requestView')->with('msgWarning', "Você precisa selecionar um ou mais produtos para fazer uma requisição!");
+    
+            } else{
+                
+                for($i = 0; $i < count($requests); $i++){
+                    if($requests[$i] <= 0){
+    
+                        return redirect()->route('requestView')->with('msgError', "A requisição do produto $name[$i] não foi feita, pois o valor inserido era invalido!");
+    
+                    } elseif($quantity[$i] < $requests[$i]){
+                         
+                        return redirect()->route('requestView')->with('msgError', "A quantidade requerida do produto $name[$i] é maior do que a quantidade disponível!");
+    
+                    } else{
+    
+                        $newQuantity = $quantity[$i] - $requests[$i];
+                        
+                        $update = Product::findOrFail($id[$i])->update(['quantity' => $newQuantity]);
+                    }
+                }
+    
+                if($update){
+    
+                    return redirect()->route('home.user')->with('msg', "Requisição de um ou mais produtos feita com sucesso!");
+    
+                } else{
+    
+                    return redirect()->route('home.user')->with('msgError', "Erro ao requisitar um ou mais produtos!");
+                }
+            }
+        } catch(Exception){
+            return redirect()->route('home.user')->with('msgError', "Erro ao fazer a requisição!");
         }
     }
 }
