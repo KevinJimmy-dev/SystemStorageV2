c8135de428621ef9e4c3027a853d9b12f5a0fe9c
kevinjimmy-dev <kevinjimmy.profissional@hotmail.com>
2022-08-28 10:44:18 -0300

    Arraging User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Exception;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,35 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = ['name', 'username', 'password', 'level', 'stats'];
+    protected $fillable = ['admin_id', 'coordinator_id', 'employee_id', 'name', 'email', 'username', 'password', 'cpf', 'phone', 'status'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password'];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $with = ['categories', 'controls', 'requests'];
 
     public function requests(){
-        return $this->hasMany('App\Models\Request');
+        return $this->hasMany(Request::class);
     }
 
     public function control(){
-        return $this->belongsTo('App\Models\Control');
+        return $this->belongsTo(Control::class);
+    }
+
+    public function categories(){
+        return $this->hasMany(Category::class);
     }
 
     public static function userLevel(){
